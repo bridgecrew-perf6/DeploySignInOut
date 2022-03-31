826555f72e63661547082ecc8e51f06b73736f0c
floKianB <kianbaban@yahoo.com>
2022-03-30 21:57:47 -0400

    none deploy eddited 004

--- signInOut/settings.py
+++ signInOut/settings.py
@@ -71,6 +71,10 @@ DATABASES = {
     }
 }
 
+import dj_database_url 
+bd_form_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(bd_form_env)
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
